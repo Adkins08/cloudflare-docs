461683d690a49680d7d113f86f1a25cd757a7574
Kian Newman-Hazel <kian@cloudflare.com>
2025-05-22 09:17:03 +0100

    fix url

--- src/components/SupportAI.tsx
+++ src/components/SupportAI.tsx
@@ -43,8 +43,8 @@ export default function SupportAI() {
 		let sources: Sources = [];
 
 		await fetchEventSource(
-			"http://localhost:8010/proxy/devdocs/ask",
-			// "https://support-ai.cloudflaresupport.workers.dev/devdocs/ask",
+			// "http://localhost:8010/proxy/devdocs/ask",
+			"https://support-ai.cloudflaresupport.workers.dev/devdocs/ask",
 			{
 				method: "POST",
 				body: JSON.stringify({
